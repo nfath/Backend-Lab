81d69f36de36aaab9280467e0cc12b8b06c95a73
Nader <nfath@yahoo.com>
2022-11-19 02:15:24 -0500

    exercise-4 done!

--- lab2/exercise-4/server.js
+++ lab2/exercise-4/server.js
@@ -4,23 +4,22 @@ const hostname = '127.0.0.1';
 const port = 3001;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
+  console.log(`url: ${req.url}`);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-    console.log(`url: ${req.url}\n`);
+  if(req.url === '/deposit') {
+      wallet.depositAmount(100);
+      res.end('Deposit request recieved');
+  }
+  else if(req.url === '/address') {
+      wallet.getAddress();
+      res.end('Address request received');
+  }
+  else {
+    res.end('Nothing here!\nChange you correct page1');
+  }
 
-    if(req.url === '/deposit') {
-        wallet.depositAmount(100);
-        res.end('Deposit request recieved');
-    }
-    else if(req.url === '/address') {
-        wallet.getAddress();
-        res.end('Address request received');
-    }
-    else {
-        res.end('Nothing here!\nChange you correct page1');
-    }
 });
 
 server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}/`));
