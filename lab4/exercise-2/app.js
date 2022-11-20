e16446f9f485dbc6de64fb37f310bffac87066c6
Nader <nfath@yahoo.com>
2022-11-19 22:38:42 -0500

    GET POST PUT DELETE added to exercise-2

---
+++ lab4/exercise-2/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+let app = express();
+
+app.listen(3002);
+
+let requestTime = (req, res, next) => {
+    req.requestTime = Date.now();
+    next();
+};
+
+app.use(requestTime);
+
+app.get("/greet", (req, res) => {
+    console.log(`GET received: ${req.requestTime}`);
+    res.send("Hello green GET!");
+});
+
+app.post("/greet", (req, res) => {
+    console.log(`POST received: ${req.requestTime}`);
+    res.send("Hello green POST!");
+});
+
+app.put("/greet", (req, res) => {
+    console.log(`PUT received: ${req.requestTime}`);
+    res.send("Hello green PUT!");
+});
+
+app.delete("/greet", (req, res) => {
+    console.log(`DELETE received: ${req.requestTime}`);
+    res.send("Hello green DELETE!");
+});
